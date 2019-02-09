50e533386037c1f24db76db3a33139ed70c3c9f5
khaledElhossiny <46382183+khaledElhossiny@users.noreply.github.com>
2019-02-09 18:00:56 +0200

    Update AddPage.php

--- View/AddPage.php
+++ View/AddPage.php
@@ -7,21 +7,5 @@
 			<input type="submit" name="addPage" value="Add Page">
 		</form>
 	</div>
-	<div id="search-div">
-		<form action="../Controller/PageController.php" method="post">
-			<label>Search</label> <input type="text" name="search">
-			<input type="submit" name="search" value="Search">
-		</form>
-		
-		<?php
-			require_once("../Contorller/PageController.php");
-			$PageController=new PageController();
-			$result=$PageController->searchPage();
-			while($row=mysqli_fetch_array($result))
-			{
-				echo "<table>"
-			}
-		?>
-	</div>
 </div>
 </html>
