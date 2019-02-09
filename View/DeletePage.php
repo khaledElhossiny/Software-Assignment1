3313f3012679d646a5fbfcd96a5e7b5e3ec6e960
khaledElhossiny <46382183+khaledElhossiny@users.noreply.github.com>
2019-02-09 20:45:55 +0200

    Add files via upload

---
+++ View/DeletePage.php
@@ -0,0 +1,43 @@
+<html>
+<div id="page-container-div">
+    <div id="add-page-div">
+        <label>Search:</label>
+        <form action="" method="post">
+            <label>Page Friendly Name</label> <input type="text" name="friendlyName"><br>
+            <input type="submit" name="search" value="Search">
+        </form>
+    </div>
+    <?php
+    require_once("../Controller/PageController.php");
+    if(!empty($_POST['search']))
+    {
+        $PageControllerObject=new PageController();
+        $result=$PageControllerObject->search($_POST['friendlyName']);
+        if(mysqli_num_rows($result)>0)
+        {
+            //display header
+            echo
+            "<table border='1' id='page_table'>
+				    <form action=''>
+					    <tr bgcolor='#CCCCCC'>
+						    <td>ID</td> <td>Address</td> <td>Friendly Name</td>
+					    </tr>";
+
+            while($rows=mysqli_fetch_array($result))
+            {
+                //display data
+                echo
+                    "<tr>"
+                    ."<td>".$rows['PageId']."</td>"
+                    ."<td>".$rows['PhysicalAdrs']."</td>"
+                    ."<td>".$rows['FriendlyName']."</td>"
+                    ."<td>".'<a href="../Controller/PageController.php?id='.$rows['PageId'].'">Delete</a>'."</td>"
+                    ."</tr>";
+            }
+            echo "</table>";
+            echo "</form>";
+        }
+    }
+    ?>
+</div>
+</html>
