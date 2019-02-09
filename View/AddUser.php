ae155678f6b73cabc841eeb2b3e969c4852d97d6
khaledElhossiny <46382183+khaledElhossiny@users.noreply.github.com>
2019-02-09 17:23:25 +0200

    Add files via upload

---
+++ View/AddUser.php
@@ -0,0 +1,11 @@
+<html>
+<div id="page-container-div">
+	<div id="add-page-div">
+		<form action="../Controller/PageController.php" method="post">
+			<label>Name</label> <input type="text" name="Name">
+			<label>Password</label> <input type="text" name="Password">
+			<input type="submit" name="addUser" value="Add">
+		</form>
+	</div>
+</div>
+</html>
